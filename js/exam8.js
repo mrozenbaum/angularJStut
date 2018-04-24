3360aaf9c610a51b2b80f5b97e78a95028cea8cd
Miriam Rozenbaum <miriamrozenbaum@miriams-mbp.attlocal.net>
2018-04-23 21:36:23 -0400

    filter practice

---
+++ js/exam8.js
@@ -0,0 +1,39 @@
+var app8 = angular.module('app8', []);
+
+app8.controller('mainCtrl', function($scope) {
+
+  $scope.students = [
+    {name : "Charlie Rozenbaum", gpa: 3.4},
+    {name: "Matan Ohad", gpa: 4.0},
+    {name: "Miriam Rozenbaum", gpa: 3.8},
+    {name: "Hofit Ohad", gpa: 3.9}
+  ];
+
+  $scope.student = {
+    gpas:[
+      {name : "Charlie Rozenbaum", gpa: 3.4},
+      {name: "Matan Ohad", gpa: 4.0},
+      {name: "Miriam Rozenbaum", gpa: 3.8},
+      {name: "Hofit Ohad", gpa: 3.9}
+    ]
+  };
+
+  $scope.currDate = new Date();
+
+  $scope.randomStr = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " 
+
+  $scope.randArray = [
+    "Tomato",
+    "Apple",
+    "Avacado",
+    "Pickles",
+    "Raisins"
+  ];
+
+  $scope.weather = [
+    {day : "Monday", rain: false},
+    {day : "Tuesday", rain: true}
+  ];
+
+
+});
